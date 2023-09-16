e4c32e5e8ce124208c5a72c43f18848748b0237d
Shinya Fujino <shf0811@gmail.com>
2023-09-16 17:48:24 +0900

    デプロイ先を Vercel に切り替え

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,13 @@
 import { defineConfig } from 'astro/config';
 
-import cloudflare from '@astrojs/cloudflare';
+import vercel from '@astrojs/vercel/serverless';
 import react from '@astrojs/react';
 import { vanillaExtractPlugin } from '@vanilla-extract/vite-plugin';
 
 // https://astro.build/config
 export default defineConfig({
   output: 'hybrid',
-  adapter: cloudflare(),
+  adapter: vercel(),
   image: {
     domains: ['images.microcms-assets.io'],
   },
